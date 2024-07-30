cb803fb43d619d0c86e4682d34b2c09b3ca4284c
dantemayson <meh.far.hdi@gmail.com>
2024-07-30 13:30:46 +0330

    first commit

---
+++ FluentUIBlazorDemo/Models/SimplePerson.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentUIBlazorDemo.Models
+{
+    public class SimplePerson
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ FluentUIBlazorDemo/Program.cs
@@ -0,0 +1,14 @@
+using FluentUIBlazorDemo;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddFluentUIComponents();
+builder.Services.AddDataGridEntityFrameworkAdapter();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
